07bb7fd29254e37b20ec575ca5b203812b483193
Jameson Taylor <jameson@ionic.io>
2023-03-13 13:24:16 -0400

    Add workaround to handle when no email client is installed;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
 import { EmailComposer } from '@ionic-enterprise/email-composer/ngx';
 
 @Component({
@@ -11,12 +12,26 @@ export class HomePage {
   constructor(private emailComposer: EmailComposer) {}
 
   async openDefault() {
-    const result = await this.emailComposer.hasClient('mailto');
-    console.log(`Device has client "mailto": ${result}`);
-    try {
-      await this.emailComposer.open({ app: 'mailto', subject: 'This is a test' });
-    } catch (e) {
-      console.log('Failed to open "mailto" client', e);
+    if (Capacitor.getPlatform() === 'ios') {
+      // Check for a configured account on iOS because hasClient('mailto') seems to always return true (iOS bug?)
+      // Currently don't know if Android has same bug, avoid checking on that plaform for now (Android needs permissons prior to invoking hasAccount()
+      const canSendEmail = await this.emailComposer.hasAccount();
+      if (!canSendEmail) {
+        console.error('No account configured for this device');
+        return;
+      }
+    }
+
+    const hasDefaultClient = await this.emailComposer.hasClient('mailto');
+    if (hasDefaultClient) {
+      console.log('Device has "mailto" client');
+      try {
+        await this.emailComposer.open({ app: 'mailto', subject: 'This is a test' });
+      } catch (e) {
+        console.error('Failed to open "mailto" client', e);
+      }
+    } else {
+      console.error('Device does NOT have "mailto" client');
     }
   }
 }
